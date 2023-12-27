78220dea1aa2d31279edbaef4795511cf117fe7e
kentcode01 <63255705+kentcode01@users.noreply.github.com>
2023-12-27 10:31:46 -0800

    Add user interface
    Add buttons for game choice and div element for displaying results.

--- rock-paper-scissor/index.js
+++ rock-paper-scissor/index.js
@@ -9,48 +9,61 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase();
     computerSelection = computerSelection.toUpperCase();
 
+
+
+    console.log(`You chose ${playerSelection}`);
+
     if(playerSelection == computerSelection) {
         return `You both chose ${playerSelection}. Its a TIE.`
     } 
     else if(playerSelection == `ROCK`) {
         if(computerSelection == `PAPER`) {
-            return `You Lose! Paper beats Rock`;
+            return `You Lose! PAPER beats ROCK`;
         } else {
-            return `You Win! Rock beats Scissor`;
+            return `You Win! ROCK beats SCISSOR`;
         }
     } 
     else if(playerSelection == `PAPER`) {
         if(computerSelection == `SCISSOR`) {
-            return `You Lose! Scissor beats Paper`;
+            return `You Lose! SCISSOR beats PAPER`;
         } else {
-            return `You Win! Paper beats Rock`;
+            return `You Win! PAPER beats ROCK`;
         }
     } else {
         // if playerSelection is SCISSOR
         if(computerSelection == `ROCK`) {
-            return `You Lose! Rock beats Scissor`;
+            return `You Lose! ROCK beats SCISSOR`;
         } else {
-            return `You Win! Scissor beats Paper`;
+            return `You Win! SCISSOR beats PAPER`;
         }
     }
 
 }
 
-function game() {
+function game(choice, results) {
+    // console.log(`this is my ${choice}`);
     wins = 0;
     losses = 0;
-    for(i = 0; i < 5; i++) {
-        choice = String(prompt(`Your choice: `));
-        result = playRound(choice, getComputerChoice());
-        console.log(result);
-        if(result.includes(`Lose`)) {
-            losses += 1;
-        } else if(result.includes(`Win`)) {
-            wins += 1;
+
+    // choice = String(prompt(`Your choice: `));
+    result = playRound(choice, getComputerChoice());
+    console.log(result);
+    if(result.includes(`Lose`)) {
+        losses += 1;
+    } else if(result.includes(`Win`)) {
+        wins += 1;
         }
-    }
+    // 
     console.log(`${wins} wins and ${losses} losses.`);
     if(wins == losses) return `The game is a TIE`;
     else if(wins > losses) return `Player Wins.`;
     else return `Computer Wins.`;
 }
+
+let buttons = document.getElementById('buttons');
+choice = "";
+buttons.addEventListener('click', (e) => {
+    choice = String(e.target.id);
+    let results = document.getElementById('results');
+    game(choice, results);
+});
