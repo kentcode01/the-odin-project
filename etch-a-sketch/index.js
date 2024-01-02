28c47975198a4f7b3f963855af11e305ed353927
kentcode01 <63255705+kentcode01@users.noreply.github.com>
2024-01-01 22:34:42 -0800

    Create an grid of 16x16 square divs
    Create inner loops and add properties in css file including flexbox.

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -1,15 +1,19 @@
-var squareSet = document.createElement(`div`);
-squareSet.setAttribute(`class`, `squareSet`)
+var grid = document.createElement(`div`);
+grid.setAttribute(`class`, `grid`);
+var setNum = 1;
 
-for(var i = 1; i < 256; i++) {
-
-    var div = document.createElement(`div`);
-    squareSet.appendChild(div);
-    div.setAttribute(`class`, `square`);
-    div.setAttribute(`id`, `div` + i);
-    div.style.border = `thin solid red`;
-    
+for(var i = 1; i <= 16; i++) {
+    var squareSet = document.createElement(`div`);
+    squareSet.setAttribute(`class`, `squareSet`);
+    squareSet.setAttribute(`id`, `set` + (setNum++));
+    for(var j = 1; j <= 16; j++) {
+        var div = document.createElement(`div`);
+        squareSet.appendChild(div);
+        div.setAttribute(`class`, `square`);
+        div.setAttribute(`id`, `square` + j);
+        div.style.border = `thin solid red`;
+    }
+    grid.appendChild(squareSet)
 }
 
-
-document.body.appendChild(squareSet);
+document.body.appendChild(grid);
