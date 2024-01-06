b846184ce0638e960748f985dbb767891b235c2e
kentcode01 <63255705+kentcode01@users.noreply.github.com>
2024-01-05 17:29:30 -0800

    Add button and change size of squares to fit grid.
    Set width and height of square by dividing grid width by user input.
    
    Previous version of program would change grid size based on squares.

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -1,19 +1,57 @@
-var grid = document.createElement(`div`);
-grid.setAttribute(`class`, `grid`);
-var setNum = 1;
+let board = document.getElementById(`board`);
+let grid = document.getElementById(`grid`); 
+let newGrid = document.getElementById(`alertButton`);
+let maxWidth = document.getElementById(`grid`).offsetWidth;
 
-for(var i = 1; i <= 16; i++) {
-    var squareSet = document.createElement(`div`);
-    squareSet.setAttribute(`class`, `squareSet`);
-    squareSet.setAttribute(`id`, `set` + (setNum++));
-    for(var j = 1; j <= 16; j++) {
-        var div = document.createElement(`div`);
-        squareSet.appendChild(div);
-        div.setAttribute(`class`, `square`);
-        div.setAttribute(`id`, `square` + j);
-        div.style.border = `thin solid red`;
+
+function createGrid(x, y) {
+    
+    grid.setAttribute(`id`, `grid`);
+    let width = (maxWidth * 1.0) / x;
+    
+    for(let i = 1; i <= x; i++) {
+
+        let squareSet = document.createElement(`div`);
+        squareSet.setAttribute(`class`, `squareSet`);
+        squareSet.setAttribute(`id`, `set` + i);
+
+        for(let j = 1; j <= y; j++) {
+
+            let div = document.createElement(`div`);
+            div.setAttribute(`class`, `square`);
+            div.setAttribute(`id`, `square` + j);
+            div.style.width = width + `px`;
+            div.style.height = width + `px`;
+            div.style.border = `thin solid black`;
+            div.addEventListener(`mouseover`, (e) => {
+                e.target.style.backgroundColor = `black`;
+            });
+            squareSet.appendChild(div);
+
+        }
+
+        grid.appendChild(squareSet)
+
     }
-    grid.appendChild(squareSet)
+
 }
 
-document.body.appendChild(grid);
+createGrid(16, 16);
+
+newGrid.addEventListener(`click`, () => {  
+    let sides = prompt(`Enter grid size. (e.g: 10 for 10x10 grid)`)
+    if(sides === null) return;
+    else if(isNaN(sides) == true) {
+        alert(`Enter an number.`);
+    } else {
+        if(sides <= 0) {
+            alert(`Enter valid number.`);
+        } else if(sides > 100) {
+            alert(`Max input is 100.`);
+        } else {
+            grid.innerHTML = "";
+            createGrid(sides, sides);
+        }
+    }
+})
+
