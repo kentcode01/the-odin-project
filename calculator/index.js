0be79d5d53e4d34d6dde16538e3db3b2c711e5f9
kentcode01 <63255705+kentcode01@users.noreply.github.com>
2024-01-18 11:23:28 -0800

    Refractor code and add statements for other buttons
    Add statements for if operators or clear button are clicked. Display results on screen.
    
    Change calculator object to variables for simplicity.

--- calculator/index.js
+++ calculator/index.js
@@ -1,13 +1,6 @@
 
 let a = ``, b = ``, operator = ``;
-
-let calculator = {
-    total: 0,
-    store: [],
-    index: 0,
-    currentDigit: ``,
-    operation: false,
-}
+let operation = false;
 
 function add(a, b) {
     return a + b;
@@ -26,23 +19,25 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
+    a = Number(a);
+    b = Number(b);
+    let calc = 0;
     if(operator === `+`) {
-        add(a, b);
+        calc = add(a, b);
     } else if(operator === `-`) {
-        subtract(a, b);
+        calc = subtract(a, b);
     } else if(operator === `*`) {
-        multiply(a, b);
+        calc = multiply(a, b);
     } else if(operator === `/`) {
-        divide(a, b);
+        calc = divide(a, b);
     }
+    console.log(calc);
+    return calc;
 } 
 
 function equals(a, b, operator) {
-    operate(a, b, operator);
+    return operate(a, b, operator);
 }
-
-// function checkValid() {
-// }
 
 let screen = document.getElementById(`screen`);
 
@@ -50,36 +45,46 @@ let digits = document.getElementsByTagName(`button`);
 for(let i = 0; i < digits.length; i++) {
     digits[i].addEventListener(`click`, (e) => {
         
-        if(e.target.parentElement.className == `digits`) {
-            // add to the curent value (calculator.currentValue)
-            
-            // if operations is false, then store into 'a'
-            // else if the operation is true, store into 'b'
-            if(calculator.operation === false) {
+        if(e.target.parentElement.className === `digits`) {
+        
+            if(operation === false) {
                 a += digits[i].textContent;
                 console.log('a:' + a);
+                screen.textContent = a;
             } else {
                 b += digits[i].textContent;
                 console.log('b:' + b);
+                screen.textContent = b;
             }
 
         }
-        else if(e.target.parentElement.className == `operators`) {
-            // store calculator.currentValue to var a
-            // and change 'operations' to true
-        }
-        
-        // console.log(e.target.parentElement.className)
+        else if(e.target.parentElement.className === `operators`) {
+            
+            console.log(digits[i].textContent);
+            operator = digits[i].textContent;
+            operation = true;
 
-        if(e.target.parentElement.className == `equals`) {
-            // if a and b has a value, and 'operations' is true,
-            // then calculate. 
-            // After calculation, store the solution to 'a'
-            // change 'operation' back to false
         }
 
-        if(e.target.parentElement.className == `clear`) {
-            // reset all of the objects and variables to default
+        if(e.target.className === `equals`) {
+            
+            screen.textContent = operate(a, b, operator);
+            a = screen.textContent;
+            b = ``;
+            operator = ``;
+            operation = false;
+
         }
+
+        if(e.target.className === `clear`) {
+            
+            screen.textContent = `0`;
+            a = ``;
+            b = ``;
+            operator = ``;
+            operation = false;
+            
+        }
+
     })
 }
