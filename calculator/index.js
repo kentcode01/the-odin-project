62749c53e89bd3ec69e3653c3b2457717ec1e888
kentcode01 <63255705+kentcode01@users.noreply.github.com>
2024-01-18 02:11:48 -0800

    Create 'calculator' object, and functions for event listeners
    Add event listener to buttons. Add if statement for 'digits' as users choice.
    
    Add to variable 'a' or 'b' depending on whether user clicked operation button.

--- calculator/index.js
+++ calculator/index.js
@@ -1,4 +1,13 @@
-let a, b, operator;
+
+let a = ``, b = ``, operator = ``;
+
+let calculator = {
+    total: 0,
+    store: [],
+    index: 0,
+    currentDigit: ``,
+    operation: false,
+}
 
 function add(a, b) {
     return a + b;
@@ -26,6 +35,51 @@ function operate(a, b, operator) {
     } else if(operator === `/`) {
         divide(a, b);
     }
+} 
+
+function equals(a, b, operator) {
+    operate(a, b, operator);
 }
 
+// function checkValid() {
+// }
 
+let screen = document.getElementById(`screen`);
+
+let digits = document.getElementsByTagName(`button`);
+for(let i = 0; i < digits.length; i++) {
+    digits[i].addEventListener(`click`, (e) => {
+        
+        if(e.target.parentElement.className == `digits`) {
+            // add to the curent value (calculator.currentValue)
+            
+            // if operations is false, then store into 'a'
+            // else if the operation is true, store into 'b'
+            if(calculator.operation === false) {
+                a += digits[i].textContent;
+                console.log('a:' + a);
+            } else {
+                b += digits[i].textContent;
+                console.log('b:' + b);
+            }
+
+        }
+        else if(e.target.parentElement.className == `operators`) {
+            // store calculator.currentValue to var a
+            // and change 'operations' to true
+        }
+        
+        // console.log(e.target.parentElement.className)
+
+        if(e.target.parentElement.className == `equals`) {
+            // if a and b has a value, and 'operations' is true,
+            // then calculate. 
+            // After calculation, store the solution to 'a'
+            // change 'operation' back to false
+        }
+
+        if(e.target.parentElement.className == `clear`) {
+            // reset all of the objects and variables to default
+        }
+    })
+}
