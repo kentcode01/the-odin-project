230eb1d4cdf50bce2863242531723378d45f5d94
kentcode01 <63255705+kentcode01@users.noreply.github.com>
2024-01-21 19:52:20 -0800

    Add functions and fix bugs for other operations
    Add checkDecimal function and if statements for checking negative, decimals, and length of number. Handle divide by zero errors.
    
    Prevent errors for operations on decimals, negative, and long numbers.

--- calculator/index.js
+++ calculator/index.js
@@ -1,9 +1,9 @@
 let screen = document.getElementById(`screen`);
 let digits = document.getElementsByTagName(`button`);
-
 let a = ``, b = ``, operator = ``;
 let operation = false;
 let focusA = true;
+let decimalUse = false;
 
 function add(a, b) {
     return a + b;
@@ -18,9 +18,15 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    
-    return a / b;
-    
+    let ans = a / b;
+    if(isDecimal(ans)) {
+        decimalUse = true;
+    }
+    return ans;
+}
+
+function isDecimal(n) {
+    return n - Math.floor(n) !== 0;
 }
 
 function operate(a, b, operator) {
@@ -42,29 +48,65 @@ function operate(a, b, operator) {
 function keypress(e, i) {
     if(e.target.parentElement.className === `digits`) {
         if(focusA === true) {
-            a += digits[i].textContent;
+            if(a.length < 9) {
+                a += digits[i].textContent;
+            }
             screen.textContent = a;
         } else {
-            b += digits[i].textContent;
+            if(b.length < 9) {
+                b += digits[i].textContent;
+            }
+            
             screen.textContent = b;
         }
     }
     else if (e.target.parentElement.className === `operators`) { 
-        
-        if(b !== `` && operator !== ``) {
-            a = operate(a, b, operator);
-            operator = digits[i].textContent;
-            b = ``;
+        if(digits[i].textContent === `+/-`) {
+            if(focusA === true && a === ``) {
+                a += `-`;
+                screen.textContent = `-`;
+            } else if(focusA === false && b === ``) {
+                b += `-`;
+                screen.textContent = `-`;
+            }      
+        } else if(digits[i].textContent === `.`) {
+            if(decimalUse === false) {
+                if(focusA === true && a !== `` && a !== `-`) {
+                    a += `.`;
+                    screen.textContent = a;
+                    decimalUse = true;
+                } else if(focusA === false && b !== `` && b !== `-`) {
+                    b += `.`;
+                    screen.textContent = b;
+                    decimalUse = true;
+                } 
+                
+            }
         } else {
-            focusA = false;
-            operator = digits[i].textContent;
-            
+            if(b !== `` && operator !== ``) {
+                a = operate(a, b, operator);              
+                operator = digits[i].textContent;
+                b = ``;
+            } else {
+                focusA = false;
+                operator = digits[i].textContent;
+            }
+            operation = true;
+            decimalUse = false;
         }
-        operation = true;
+            
         
             
-    } else if (e.target.className === `equals`) {      
-        screen.textContent = operate(a, b, operator);
+    } else if (e.target.className === `equals`) {  
+        let temp =  operate(a, b, operator);
+        if(String(temp).length > 9) {
+            if(isDecimal(temp)) {
+                temp = String(temp).substring(0, 8);
+            } else {
+                temp = String(temp).substring(0, 8).split(``).splice(1, 1, `.`).join(``);
+            }
+        }   
+        screen.textContent = temp;
         a = screen.textContent;
         b = ``;
         operator = ``;
@@ -72,8 +114,9 @@ function keypress(e, i) {
     } else if (e.target.className === `clear`) {      
         clear();
     }
+   
 
-    console.log(`a: ${a}, b: ${b}, operator: ${operator}, focusA: ${focusA}, operation: ${operation}`)
+    // console.log(`a: ${a}, b: ${b}, operator: ${operator}, focusA: ${focusA}, operation: ${operation}`)
 
 }
 
@@ -84,11 +127,17 @@ function clear() {
     operator = ``;
     focusA = true; 
     operation = false;
+    decimalUse = false;
 }
 
 for(let i = 0; i < digits.length; i++) {
     digits[i].addEventListener(`click`, (e) => {
         keypress(e, i);
+
+        if(a === `Infinity`) {
+            a = `ERROR`;
+            screen.textContent = a;
+        }
     });
 }
 
