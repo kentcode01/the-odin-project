5e614eb7cd20986dcf1be57e59a24cf36c22304a
kentcode01 <63255705+kentcode01@users.noreply.github.com>
2024-03-05 11:11:39 -0800

    Add dialog element, functions, and event listeners
    Create functions and event listeners for the form and buttons.

--- project-library/index.js
+++ project-library/index.js
@@ -1,3 +1,37 @@
+
+const dialog = document.querySelector(`dialog`);
+const showBtn = document.getElementById(`show`);
+const closeBtn = document.getElementById(`close`);
+const submitBtn = document.getElementById(`submit`);
+const form = document.querySelector(`form`);
+let bookList = document.getElementById(`book-list`);
+
+
+showBtn.addEventListener(`click`, () => {
+    dialog.showModal();
+});
+
+closeBtn.addEventListener(`click`, () => {
+    dialog.close();
+});
+
+form.addEventListener(`submit`, (e) => {
+    e.preventDefault();
+
+    const checkbox = document.getElementById(`read`);
+    let checkboxTxt = ``;
+    if(checkbox.checked){
+        checkboxTxt = `read`;
+    }
+
+    let currbook = new Book(document.getElementById(`title`).value, document.getElementById(`author`).value,
+    document.getElementById(`pages`).value, checkboxTxt);
+
+    addBookLibrary(currbook);
+    
+    dialog.close();
+})
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -5,14 +39,34 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-
-    this.info = function() {
-        // console.log(`${this.title} by ${this.author}, ${pages}, ${read}`);
-    }
 }
+
 
 function addBookLibrary(book) {
     myLibrary.push(book)
+
+    let bookDiv = document.createElement(`div`);
+    bookDiv.className = `book-div`;
+    let bookText = document.createElement(`p`);
+    let bookRemove = document.createElement(`button`);
+
+    bookRemove.className = `remove`;
+    bookRemove.textContent = `remove`;
+    bookRemove.style.width = `4rem`;
+    bookRemove.style.height = `1.5rem`;
+    bookRemove.style.marginLeft = `2rem`;
+    bookDiv.style.display = `flex`;
+    bookDiv.style.alignItems = `center`;
+
+    bookRemove.addEventListener(`click`, (e) => {
+        e.target.parentElement.remove();
+    })
+
+    bookText.textContent = (`${book.title} by ${book.author}, ${book.pages}, ${book.read}`);
+
+    bookDiv.appendChild(bookText);
+    bookDiv.appendChild(bookRemove);
+    bookList.appendChild(bookDiv);
 }
 
 function printBooks(library) {
@@ -22,8 +76,8 @@ function printBooks(library) {
 }
 
 addBookLibrary(new Book(`The Hobbit`, `J.R.R Tolkien`, 295, `read`));
-addBookLibrary(new Book(`Animal Farm`, `George Orwell`, 92, `not read`));
+addBookLibrary(new Book(`Animal Farm`, `George Orwell`, 92, ``));
 addBookLibrary(new Book(`Don Quixote`, `Miguel de Cervantes`, 1072, `read`));
 
+printBooks(myLibrary);
 
-printBooks(myLibrary)
