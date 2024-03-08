def857558234a36c68322f227bcf7f4bc98b8cf8
kentcode01 <63255705+kentcode01@users.noreply.github.com>
2024-03-08 11:41:44 -0800

    Create id, and function to iterate array.
    Add loop to create elements, event listeners, and adding properties.
    
    Call iterateLibrary function to refresh array and DOM each time book is added or removed.

--- project-library/index.js
+++ project-library/index.js
@@ -5,7 +5,7 @@ const closeBtn = document.getElementById(`close`);
 const submitBtn = document.getElementById(`submit`);
 const form = document.querySelector(`form`);
 let bookList = document.getElementById(`book-list`);
-
+const myLibrary = [];
 
 showBtn.addEventListener(`click`, () => {
     dialog.showModal();
@@ -27,57 +27,59 @@ form.addEventListener(`submit`, (e) => {
     let currbook = new Book(document.getElementById(`title`).value, document.getElementById(`author`).value,
     document.getElementById(`pages`).value, checkboxTxt);
 
-    addBookLibrary(currbook);
+    addBookToLibrary(currbook);
     
     dialog.close();
 })
-
-const myLibrary = [];
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.key = (title + pages).split(``).join(``);
 }
 
-
-function addBookLibrary(book) {
+function addBookToLibrary(book) {
     myLibrary.push(book)
-
-    let bookDiv = document.createElement(`div`);
-    bookDiv.className = `book-div`;
-    let bookText = document.createElement(`p`);
-    let bookRemove = document.createElement(`button`);
-
-    bookRemove.className = `remove`;
-    bookRemove.textContent = `remove`;
-    bookRemove.style.width = `4rem`;
-    bookRemove.style.height = `1.5rem`;
-    bookRemove.style.marginLeft = `2rem`;
-    bookDiv.style.display = `flex`;
-    bookDiv.style.alignItems = `center`;
-
-    bookRemove.addEventListener(`click`, (e) => {
-        e.target.parentElement.remove();
-    })
-
-    bookText.textContent = (`${book.title} by ${book.author}, ${book.pages}, ${book.read}`);
-
-    bookDiv.appendChild(bookText);
-    bookDiv.appendChild(bookRemove);
-    bookList.appendChild(bookDiv);
+    iterateLibrary();
 }
 
-function printBooks(library) {
-    for(let i = 0; i < library.length; i++) {
-        console.log(`${library[i].title} by ${library[i].author}, ${library[i].pages}, ${library[i].read}`);
+function iterateLibrary() {
+    bookList.innerHTML = ``;
+    for(let i = 0; i < myLibrary.length; i++) {
+        let bookDiv = document.createElement(`div`);
+        bookDiv.className = `book-div`;
+        let bookText = document.createElement(`p`);
+        let bookRemove = document.createElement(`button`);
+        
+        bookRemove.className = `remove`;
+        bookRemove.textContent = `remove`;
+        bookRemove.style.width = `4rem`;
+        bookRemove.style.height = `1.5rem`;
+        bookRemove.style.marginLeft = `2rem`;
+        bookDiv.style.display = `flex`;
+        bookDiv.style.alignItems = `center`;
+        bookDiv.setAttribute(`id`, `${myLibrary[i].title}${myLibrary[i].pages}`.split(``).join(``));
+        bookRemove.addEventListener(`click`, (e) => {
+            console.log(e.target.parentElement)
+            let idAttribute = (e.target.parentElement).getAttribute(`id`);
+            myLibrary.splice(myLibrary.findIndex(i => i.key === idAttribute), 1);
+            e.target.parentElement.remove();
+        })
+
+        bookText.textContent = (`${myLibrary[i].title} by ${myLibrary[i].author}, ${myLibrary[i].pages}, ${myLibrary[i].read}`);
+
+        bookDiv.appendChild(bookText);
+        bookDiv.appendChild(bookRemove);
+        bookList.appendChild(bookDiv);
     }
 }
 
-addBookLibrary(new Book(`The Hobbit`, `J.R.R Tolkien`, 295, `read`));
-addBookLibrary(new Book(`Animal Farm`, `George Orwell`, 92, ``));
-addBookLibrary(new Book(`Don Quixote`, `Miguel de Cervantes`, 1072, `read`));
 
-printBooks(myLibrary);
+myLibrary.push(new Book(`The Hobbit`, `J.R.R Tolkien`, 295, `read`))
+myLibrary.push(new Book(`Animal Farm`, `George Orwell`, 92, ``));
+myLibrary.push(new Book(`Don Quixote`, `Miguel de Cervantes`, 1072, `read`));
+iterateLibrary();
+
 
