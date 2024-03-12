9a784aa68fe3a547c417ae74ff1e2f45bfdfc77d
kentcode01 <63255705+kentcode01@users.noreply.github.com>
2024-03-12 00:07:25 -0700

    Create tic-tac-toe game for console
    Add functions to play game, check for winners, print grid, and clear board.
    
    Make simple console game as a foundation for rest of the program.

---
+++ tic-tac-toe/index.js
@@ -0,0 +1,77 @@
+// let game = [`X`, `O`, `X`, `X`, `X`, `O`, `O`, `X`, `X`];
+let game = [``, ``, ``, ``, ``, ``, ``, ``, ``];
+          // 0 1 2 
+          // 3 4 5 
+          // 6 7 8 
+function clearGame() {
+    for(let i = 0; i < game.length; i++) {
+        game[i] = ``;
+    }
+}
+
+function checkWin() {
+    const sets = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for(let i = 0; i < sets.length; i++) {
+        let arr = sets[i];
+
+        if(game[arr[0]] && game[arr[0]] === game[arr[1]] && game[arr[1]] === game[arr[2]]) {
+            return true;
+        }
+    }
+    printGrid();
+    return false;
+}
+
+function printGrid() {
+    let gameString = ``;
+    console.log(`GAME BOARD`)
+    console.log(game.length);
+    for(let i = 0; i < game.length; i++) {
+        gameString += (game[i] + `|`);
+        if((i + 1) % 3 === 0) {
+            gameString += `\n`;
+        }
+    }
+    console.log(gameString);
+}
+
+function playGame() {
+    let player = true;
+
+    let turns = 0;
+
+    while(turns < 9 || checkWin() === true) {
+        console.log(`Turn ${turns}`)
+        let option = prompt(`user choice: `);
+        if(option === `Q`) {
+            clearGame();
+            return;
+        }
+        if(game[option] === ``) {
+            if(player === true) {
+                game[option] = `X`;
+            } else {
+                game[option] = `O`;
+            }
+            turns++;
+            player = !player;
+        }
+        if(checkWin() === true) {
+            printGrid();
+            {player? console.log(`Player Wins`) : console.log(`CPU Wins`)};
+            clearGame();
+            return;
+        }
+    }
+    
+}
